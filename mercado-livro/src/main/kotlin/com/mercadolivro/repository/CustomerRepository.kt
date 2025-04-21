7ec36ac5946068781f8265eafe8c5da591fd5eec
Franklin Percicotte <frankpercicotte@gmail.com>
2025-04-20 23:10:00 -0300

    Inclued pageable

--- mercado-livro/src/main/kotlin/com/mercadolivro/repository/CustomerRepository.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/repository/CustomerRepository.kt
@@ -1,10 +1,14 @@
 package com.mercadolivro.repository
 
 import com.mercadolivro.model.CustomerModel
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 
-interface CustomerRepository : CrudRepository<CustomerModel, Int> {
+//interface CustomerRepository : CrudRepository<CustomerModel, Int> {
+interface CustomerRepository : JpaRepository<CustomerModel, Int> {
 
-    fun findByNameContaining(name: String): List<CustomerModel>
+    fun findByNameContaining(name: String, pageable: Pageable): Page<CustomerModel>
 
 }
