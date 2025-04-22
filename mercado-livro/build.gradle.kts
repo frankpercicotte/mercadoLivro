df494f6c80f98fd5bf7b50e7d75444fbde2a8cd8
Franklin Percicotte <frankpercicotte@gmail.com>
2025-04-21 21:33:10 -0300

    Updating build.gradle

--- mercado-livro/build.gradle.kts
+++ mercado-livro/build.gradle.kts
@@ -1,11 +1,12 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.3"
+	id("org.springframework.boot") version "2.7.0"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.4.30"
-	kotlin("plugin.spring") version "1.4.30"
-	kotlin("plugin.jpa") version "1.4.30"
+	kotlin("jvm") version "2.1.20"
+	kotlin("plugin.spring") version "2.1.20"
+	kotlin("plugin.jpa") version "2.1.20"
 }
 
 group = "com.mercadolivro"
@@ -17,6 +18,7 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -30,12 +32,19 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
+kotlin {
+	compilerOptions {
+		jvmTarget.set(JvmTarget.JVM_17)
+		javaParameters.set(true)
 	}
 }
+
+//tasks.withType<KotlinCompile> {
+//	kotlinOptions {
+//		freeCompilerArgs = listOf("-Xjsr305=strict")
+//		jvmTarget = "11"
+//	}
+//}
 
 tasks.withType<Test> {
 	useJUnitPlatform()
