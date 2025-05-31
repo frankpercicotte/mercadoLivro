99aeb6e59c5048b13a858800959b70f87e6c9c73
Franklin Percicotte <frankpercicotte@gmail.com>
2025-05-30 22:21:39 -0300

    Included http authorize

--- mercado-livro/src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
@@ -2,6 +2,7 @@ package com.mercadolivro.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 
@@ -10,6 +11,10 @@ import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 class SecurityConfig() {
+    //PUBLIC_MATCHERS used to open url with all type: POST,GET,PUT...
+    private val PUBLIC_MATCHERS = arrayOf<String>()
+    private val PUBLIC_POST_MATCHERS = arrayOf("/customer")
+    private val PUBLIC_GET_MATCHERS = arrayOf("/books","/books/actives")
 
     @Bean
     fun bCryptPasswordEncoder(): BCryptPasswordEncoder{
@@ -19,6 +24,11 @@ class SecurityConfig() {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http.cors().and().csrf().disable()
+        http.authorizeRequests()
+            .antMatchers(*PUBLIC_MATCHERS).permitAll()
+            .antMatchers(HttpMethod.POST,*PUBLIC_POST_MATCHERS).permitAll()
+            .antMatchers(HttpMethod.GET,*PUBLIC_GET_MATCHERS).permitAll()
+            .anyRequest().authenticated()
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         return http.build()
     }
